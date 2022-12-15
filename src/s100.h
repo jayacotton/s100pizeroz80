1c6fdb299f71d35f4c5cdc5dae6346ada57ec35a
Jay Cotton <lbmgmusic@gmail.com>
2022-12-14 21:03:55 -0800

    define a bunch of stuff

---
+++ src/s100.h
@@ -0,0 +1,54 @@
+/* s100 bus pin definitions */
+
+/*
+1       +8 V          51      +8V
+2       +16 V         52      -16V
+3       XRDY          53      GND
+4       /VI0          54      /S CLR
+5       /VI1          55      /DMA0
+6       /VI2          56      /DMA1
+7       /VI3          57      /DMA2
+8       /VI4          58      /SXTRQ
+9       /VI5          59      A19
+10      /VI6          60      /SIXTN
+11      /VI7          61      A20
+12      /NMI          62      A21
+13      /PWRFAIL      63      A22
+14      /DMA3         64      A23
+15      A18           65      ---
+16      A16           66      ---
+17      A17           67      /PHANTOM
+18      /SDSB         68      MWRT
+19      /CDSB         69      rsvd
+20      GND           70      GND
+21      ---           71      rsvd
+22      /ADSB         72      RDY
+23      /DODSB        73      /INT
+24      phi2,phi0     74      /HOLD
+25      /PSTVAL       75      /RESET
+26      PHLDA         76      PSYNC
+27      rsvd          77      /PWR
+28      rsvd          78      PDBIN
+29      A5            79      A0
+30      A4            80      A1
+31      A3            81      A2
+32      A15           82      A6
+33      A12           83      A7
+34      A9            84      A8
+35      DO1|D1        85      A13
+36      DO0|D0        86      A14
+37      A10           87      A11
+38      DO4|D4        88      DO2|D2
+39      DO5|D5        89      DO3|D3
+40      DO6|D6        90      DO7|D7
+41      DI2|D10       91      DI4|D12
+42      DI3|D11       92      DI5|D13
+43      DI7|D15       93      DI6|D14
+44      SM1           94      DI1|D9
+45      SOUT          95      DI0|D8
+46      SINP          96      SINTA
+47      SMEMR         97      /SWO
+48      SHLTA         98      /ERROR
+49      CLOCK         99      /POC
+50      GND           100     GND
+ */
